d8aa6ccbcdc35e74e658045a1739268523269bf4
arya2 <arya281996@gmail.com>
2024-06-15 19:20:10 +0530

    New updates

--- src/test/java/Testpkg/Logintest.java
+++ src/test/java/Testpkg/Logintest.java
@@ -4,17 +4,24 @@ import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
+import java.awt.Window;
+import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.Duration;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -22,6 +29,7 @@ import org.testng.annotations.Test;
 
 import Basepkg.Baseclass;
 import Pagepkg.Loginpage;
+import net.bytebuddy.build.Plugin.Factory.UsingReflection.Priority;
 import utilities.Exelutilities;
 
 
@@ -58,8 +66,11 @@ public class Logintest extends Baseclass {
 		
 		
 	}
+	
+	
+	
 	@Test(priority = 2)
-	public void wishlist() throws InterruptedException
+	public void carttest() throws InterruptedException
 	{
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
 		WebElement category=driver.findElement(By.xpath("//*[@id=\"HeaderMenu-shop-by-category\"]/span"));
@@ -68,56 +79,83 @@ public class Logintest extends Baseclass {
 		act.perform();
 		
 		driver.findElement(By.xpath("//*[@id=\"HeaderMenu-shop-by-category-all\"]")).click();
+		driver.findElement(By.xpath("/html/body/main/div[2]/div/div/div/div/ul/li[1]/div/div/div[2]/div[1]/h3/a")).click();
+		driver.findElement(By.xpath("/html/body/main/section/section/div/div[2]/product-info/delivery-time/form/input[1]")).sendKeys("683562");
+		driver.findElement(By.xpath("/html/body/main/section/section/div/div[2]/product-info/delivery-time/form/button")).click();
 		Thread.sleep(3000);
+		driver.findElement(By.xpath("/html/body/main/section/section/div/div[2]/product-info/div[4]/product-form/form/div/div[2]/button[2]")).click();
+	
+		//driver.findElement(By.xpath("html/body/main/div[2]/div/div/div/div/ul/li[1]/button")).click();
+		//driver.findElement(By.xpath("/html/body/div[2]/sticky-header/header/div/a[3]/div[1]/span")).click();
+	
+	//	driver.findElement(By.className("swym-add-to-cart-btn swym-button swym-button-1 swym-is-button swym-bg-2 swym-color-4 swym-is-button")).click();
+	
+Set<String>	hSet=driver.getWindowHandles();
+Iterator wIterator=hSet.iterator();
+String pString= (String) wIterator.next();
+String cString=(String) wIterator.next();
+System.out.println(pString);
+System.out.println(cString);
+driver.switchTo().window(cString);
+Thread.sleep(3000);
+driver.findElement(By.id("cart-notification-button")).click();
+//driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/cart-items/form/div/div/table/tbody/tr[4]/td[2]/div[3]/cart-remove-button/a")).click();
+
 		
 	
 
-		List<WebElement>list=driver.findElements(By.xpath("//*[@id=\"product-grid\"]/li"));
 	
-		
-		for(WebElement e:list)
-		{
-			String textString=e.getAttribute("href");
-			String textString2=e.getText();
-			System.out.println(textString2);
+				
+			
 			
 		
-			
-			if(textString.equals("https://www.giva.co/products/silver-snowflake-earrings?_pos=2&_sid=6cd0e926f&_ss=r"))
-			{
-				
-				e.click();
-				
-				
-				
-			}
-			
-			
-		}	
 		
 
-}	
-	
+}
 	@Test(priority = 3)
-	public void wishcart() throws Exception
+	public void logo() throws Exception
+	{
+		Thread.sleep(3000);
+		boolean logo=driver.findElement(By.xpath("//*[@id=\"shopify-section-sections--16317651026082__header\"]/sticky-header/header/a/div/img")).isDisplayed();
+		if(logo)
+		{
+			System.out.println("logo is displayed");
+			
+		}
+		else {
+			System.out.println("logo is not displayed");
+			
+		}
+	}
+	@Test(priority = 4)
+	public void screenshot() throws Exception
+	{
+		File srcFile=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileHandler .copy(srcFile,new File("D://d2.png"));
+
+			
+			WebElement element=driver.findElement(By.xpath("/html/body/main/div[2]/div[1]/cart-items/form/div/div/table/tbody/tr[1]/td[2]/div[3]/move-to-wishlist-button"));
+		File bFile=element.getScreenshotAs(OutputType.FILE);
+		FileHandler .copy(bFile, new File("./Screenshot123/new23.png"));
+			
+	}
+	@Test(priority = 5)
+	public void search() throws Exception
 	{
 		
-		Wait<WebDriver> wait2 = new WebDriverWait(driver, Duration.ofSeconds(30));
-		WebElement wish2=wait2.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/main/div[2]/div/div/div/div/ul/li[1]/button")));
-       wish2.click();
-		
-		
-		
-		
+	
+		driver.findElement(By.xpath("/html/body/div[3]/sticky-header/header/predictive-search/form/div[1]/input[1]")).sendKeys("gold rings");
+		driver.findElement(By.xpath("/html/body/div[3]/sticky-header/header/predictive-search/form/div[2]/div/div[1]/div[1]/div/ul/li[2]/a/div/p/mark")).click();
+		driver.findElement(By.xpath("/html/body/main/section/div/div[2]/div/div/ul/li[1]/button")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("/html/body/div[3]/sticky-header/header/div/a[2]")).click();
 	}
 	
 	
+	
 
-	
-	
-	
-	
-/*@Test(priority = 3)
+
+	@Test(priority = 6)
 	public void test3() throws Exception {
 		List<WebElement>list=driver.findElements(By.tagName("a"));
 		System.out.println(list.size());
@@ -150,32 +188,46 @@ public class Logintest extends Baseclass {
 			// TODO: handle exception
 		}
 		
-		
-
-}*/
-	
-	@Test(priority = 4)
-	public void testlogo()
+	}
+	@Test(priority = 7)
+	public void logout()
 	{
-
-		boolean element= driver.findElement(By.xpath("/html/body/div[2]/sticky-header/header/a/div/img")).isDisplayed();
-		if(element==true)
-		{
-			System.out.println("passed logo is present");
-		}
-		 else {
-			 System.out.println("failed");
-			
-		}
-		
-	
+		driver.findElement(By.xpath("/html/body/main/div/div/div[3]/a")).click();
 	}
+		
+
+}
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 			
 		
 		
 		
 
-
-	}
+	
 
